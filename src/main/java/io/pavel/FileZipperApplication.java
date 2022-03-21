712a431ee2dd3cb25f082de44d7f6b28bb58ad73
agima_Shcherbakov <TtxAR25L>
2022-03-21 23:05:34 +0300

    init commit

---
+++ src/main/java/io/pavel/FileZipperApplication.java
@@ -0,0 +1,13 @@
+package io.pavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileZipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileZipperApplication.class, args);
+	}
+
+}
