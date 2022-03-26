a85793efe0e179486af18a6bc39534cf2dd5e33f
agima_Shcherbakov <TtxAR25L>
2022-03-26 21:03:26 +0300

    change host address from headers to remote address from request

--- src/main/java/io/pavel/controller/FileController.java
+++ src/main/java/io/pavel/controller/FileController.java
@@ -2,18 +2,16 @@ package io.pavel.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.pavel.service.ZipService;
-import io.pavel.utils.Utils;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,8 +22,8 @@ public class FileController {
     private final ZipService zipService;
 
     @PostMapping(produces = "application/zip")
-    public void zipFiles(@RequestParam("file") MultipartFile[] files, HttpServletResponse response, @RequestHeader HttpHeaders headers) throws IOException {
-        String clientHost = Utils.getClientHost(headers);
+    public void zipFiles(@RequestParam("file") MultipartFile[] files, HttpServletResponse response, HttpServletRequest requests) throws IOException {
+        String clientHost = requests.getRemoteHost();
         zipService.zipFilesAndWriteToResponse(files, response, clientHost);
     }
 }
